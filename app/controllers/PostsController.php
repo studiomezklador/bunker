9b5f91f42eb8e82eabc7e0db9d310a49149092ab
ZEF on Mint17 <se@mezklador.com>
2014-08-08 01:08:52 +0200

    Correctif sur PostsController->index().

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -10,7 +10,7 @@ class PostController extends BaseController{
 
 	public function index()
 	{
-		$posts = Posts:get();
+		$posts = Posts::get();
 		return View::make('posts.index', compact('posts'));
 	}
 }
